3d8e31b4e7c1f249dc52d4a2c00aa5a5705e6db9
dhkatz <dhkatz.16@gmail.com>
2022-08-23 00:42:24 -0700

    imvu.js is now a monorepo

--- packages/client/test/controllers/ProductController.test.ts
+++ packages/client/test/controllers/ProductController.test.ts
@@ -1,38 +1,32 @@
-import { ProductController, Product, User, Client } from '../../src';
+import { ProductController, User, Client } from '../../src';
 
 describe('ProductController.test.ts', () => {
   const client = new Client();
 
-  test('Invalid ID Returns null', async (done) => {
+  test('Invalid ID Returns null', async () => {
     const controller = new ProductController(client);
 
     const product = await controller.fetch(1);
 
     expect(product).toBe(null);
-
-    done();
   });
 
-  test('Invalid Creator Returns Null', async (done) => {
+  test('Invalid Creator Returns Null', async () => {
     const controller = new ProductController(client);
 
     const products = await controller.search({ creator: 'x124za' });
 
     expect(products).toHaveLength(0);
-
-    done();
   });
 
-  test('Product creator retrieves User instance', async (done) => {
+  test('Product creator retrieves User instance', async () => {
     const controller = new ProductController(client);
 
-    const product: Product = await controller.fetch(33773936);
+    const product = await controller.fetch(33773936);
 
-    const creator = product.creator;
+    const creator = await product?.creator();
 
     expect(creator).toBeInstanceOf(User);
-    expect(creator.registered).toBe(1313110378);
-
-    done();
+    expect(creator?.registered).toBe(1313110378);
   });
 });
--- packages/client/test/controllers/UserController.test.ts
+++ packages/client/test/controllers/UserController.test.ts
@@ -1,37 +1,31 @@
 import { UserController, Client } from '../../src';
-import {User} from "../../src";
+import { User } from '../../src';
 
 describe('UserController.test.ts', () => {
   const client = new Client();
 
-  it('return null on an invalid ID', async (done) => {
+  it('return null on an invalid ID', async () => {
     const controller = new UserController(client);
 
     const user = await controller.fetch(1);
 
     expect(user).toBe(null);
-
-    done();
   });
 
-  it('should return null on invalid username', async (done) => {
+  it('should return null on invalid username', async () => {
     const controller = new UserController(client);
 
     const user = await controller.search({ username: 'x124za' });
 
     expect(user).toHaveLength(0);
-
-    done();
   });
 
-  it('should return a user on ID lookup', async (done) => {
+  it('should return a user on ID lookup', async () => {
     const controller = new UserController(client);
 
     const user = await controller.fetch(54313769);
 
     expect(user).toBeInstanceOf(User);
-    expect(user.username).toBe('ulIr');
-
-    done();
+    expect(user?.username).toBe('ulIr');
   });
 });
